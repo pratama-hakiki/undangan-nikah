fe300bb2a04d1354ff042f91fa293184fc005690
Pratama Aji P <pratamaajip@Pratamas-MacBook-Pro.local>
2025-03-26 22:08:56 +0700

    2 commit

--- src/assets/data/data.js
+++ src/assets/data/data.js
@@ -1,126 +1,127 @@
 export const data = {
-    bride: {
-        L: {
-            id: 1,
-            name: 'Lorem Ipsum',
-            child: 'Putra ke lorem',
-            father: 'Lorem',
-            mother: 'Ipsum',
-            image: './src/assets/images/cowo.png'
-        },
-        P: {
-            id: 2,
-            name: 'Ipsum Lorem',
-            child: 'Putri ke lorem',
-            father: 'Lorem',
-            mother: 'Ipsum',
-            image: './src/assets/images/cewe.png'
-        },
-
-        couple: './src/assets/images/couple.png'
+  bride: {
+    L: {
+      id: 1,
+      name: "Lorem Ipsum",
+      child: "Putra ke lorem",
+      father: "Lorem",
+      mother: "Ipsum",
+      image: "./src/assets/images/cowo.png",
+    },
+    P: {
+      id: 2,
+      name: "Ipsum Lorem",
+      child: "Putri ke lorem",
+      father: "Lorem",
+      mother: "Ipsum",
+      image: "./src/assets/images/cewe.png",
     },
 
-    time: {
-        marriage: {
-            year: '2030',
-            month: 'November',
-            date: '14',
-            day: 'Kamis',
-            hours: {
-                start: '08.00',
-                finish: 'Selesai'
-            }
-        },
-        reception: {
-            year: '2024',
-            month: 'November',
-            date: '14',
-            day: 'Kamis',
-            hours: {
-                start: '11.00',
-                finish: 'Selesai'
-            }
-        },
-        address: 'Kp. Lorem, RT 000/ RW 000, Desa.Lorem, Kec.Ipsum, Kab.Lorem, Lorem (1234)'
+    couple: "./src/assets/images/couple.png",
+  },
+
+  time: {
+    marriage: {
+      year: "2030",
+      month: "November",
+      date: "14",
+      day: "Kamis",
+      hours: {
+        start: "08.00",
+        finish: "Selesai",
+      },
     },
-
-    link: {
-        calendar: 'https://calendar.app.google/ibwoBW7YTjKYKi2F8',
-        map: 'https://maps.app.goo.gl/r6QKvq4b8YRUPdUd8',
+    reception: {
+      year: "2024",
+      month: "November",
+      date: "14",
+      day: "Kamis",
+      hours: {
+        start: "11.00",
+        finish: "Selesai",
+      },
     },
+    address:
+      "Kp. Lorem, RT 000/ RW 000, Desa.Lorem, Kec.Ipsum, Kab.Lorem, Lorem (1234)",
+  },
 
-    galeri: [
-        {
-            id: 1,
-            image: './src/assets/images/1.png'
-        },
-        {
-            id: 2,
-            image: './src/assets/images/2.png'
-        },
-        {
-            id: 3,
-            image: './src/assets/images/3.png'
-        },
-        {
-            id: 4,
-            image: './src/assets/images/4.png'
-        },
-        {
-            id: 5,
-            image: './src/assets/images/5.png'
-        }
-    ],
+  link: {
+    calendar: "https://calendar.app.google/ibwoBW7YTjKYKi2F8",
+    map: "https://maps.app.goo.gl/r6QKvq4b8YRUPdUd8",
+  },
 
-    bank: [
-        {
-            id: 1,
-            name: 'Lorem Ipsum',
-            icon: './src/assets/images/bca.png',
-            rekening: '12345678'
-        },
-        {
-            id: 2,
-            name: 'Ipsum Lorem',
-            icon: './src/assets/images/bri.png',
-            rekening: '12345678'
-        },
-    ],
+  galeri: [
+    {
+      id: 1,
+      image: "./src/assets/images/1.png",
+    },
+    {
+      id: 2,
+      image: "./src/assets/images/2.png",
+    },
+    {
+      id: 3,
+      image: "./src/assets/images/3.png",
+    },
+    {
+      id: 4,
+      image: "./src/assets/images/4.png",
+    },
+    {
+      id: 5,
+      image: "./src/assets/images/5.png",
+    },
+  ],
 
-    audio: './src/assets/audio/wedding.mp3',
+  bank: [
+    {
+      id: 1,
+      name: "Lorem Ipsum",
+      icon: "./src/assets/images/bca.png",
+      rekening: "12345678",
+    },
+    {
+      id: 2,
+      name: "Ipsum Lorem",
+      icon: "./src/assets/images/bri.png",
+      rekening: "12345678",
+    },
+  ],
 
-    api: 'https://script.google.com/macros/s/AKfycbyMoiBc_Q-hpaIk4F7fImZiP5azKAwlRlp-mAGVDVrfkG-pg0_WqaoszR8878NatKuJNw/exec',
+  audio: "./src/assets/audio/wedding.mp3",
 
-    navbar: [
-        {
-            id: 1,
-            teks: 'Home',
-            icon: 'bx bxs-home-heart',
-            path: '#home',
-        },
-        {
-            id: 2,
-            teks: 'Mempelai',
-            icon: 'bx bxs-group',
-            path: '#bride',
-        },
-        {
-            id: 3,
-            teks: 'Tanggal',
-            icon: 'bx bxs-calendar-check',
-            path: '#time',
-        },
-        {
-            id: 4,
-            teks: 'Galeri',
-            icon: 'bx bxs-photo-album',
-            path: '#galeri',
-        },
-        {
-            id: 5,
-            teks: 'Ucapan',
-            icon: 'bx bxs-message-rounded-dots',
-            path: '#wishas',
-        },
-    ],
-}
+  api: "https://script.google.com/macros/s/AKfycbzojOOgPXl_ufTNVik-vK7Eds6fE-jGRKVcN-MFA5m81U5tZIZaiUJLVimOWLZfu8VeJA/exec",
+
+  navbar: [
+    {
+      id: 1,
+      teks: "Home",
+      icon: "bx bxs-home-heart",
+      path: "#home",
+    },
+    {
+      id: 2,
+      teks: "Mempelai",
+      icon: "bx bxs-group",
+      path: "#bride",
+    },
+    {
+      id: 3,
+      teks: "Tanggal",
+      icon: "bx bxs-calendar-check",
+      path: "#time",
+    },
+    {
+      id: 4,
+      teks: "Galeri",
+      icon: "bx bxs-photo-album",
+      path: "#galeri",
+    },
+    {
+      id: 5,
+      teks: "Ucapan",
+      icon: "bx bxs-message-rounded-dots",
+      path: "#wishas",
+    },
+  ],
+};
