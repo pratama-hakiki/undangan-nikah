1ce25d801edcae3ccb255abc470cbc522c390eb8
Pratama Aji P <pratamaajip@Pratamas-MacBook-Pro.local>
2025-03-30 16:45:49 +0700

    update images 2

--- src/assets/data/data.js
+++ src/assets/data/data.js
@@ -53,23 +53,23 @@ export const data = {
   galeri: [
     {
       id: 1,
-      image: "./src/assets/images/photo_1_2025-03-30_14-32-42.jpg",
+      image: "./src/assets/images/photo_1_2025-03-30_16-44-29.jpg",
     },
     {
       id: 2,
-      image: "./src/assets/images/photo_2_2025-03-30_14-32-42.jpg",
+      image: "./src/assets/images/photo_2_2025-03-30_16-44-29.jpg",
     },
     {
       id: 3,
-      image: "./src/assets/images/photo_5_2025-03-30_14-32-42.jpg",
+      image: "./src/assets/images/photo_3_2025-03-30_16-44-29.jpg",
     },
     {
       id: 4,
-      image: "./src/assets/images/photo_4_2025-03-30_14-32-42.jpg",
+      image: "./src/assets/images/photo_4_2025-03-30_16-44-29.jpg",
     },
     {
       id: 5,
-      image: "./src/assets/images/photo_3_2025-03-30_14-32-42.jpg",
+      image: "./src/assets/images/photo_5_2025-03-30_16-44-29.jpg",
     },
   ],
 
